8850535f7587cd2cda00ccd062a2e8b6668f8af7
rodrigovsousa <rodrigovsousa92@gmail.com>
2021-09-16 09:56:24 -0300

    adicionando tabelas

---
+++ db-coffeetime/src/main/java/g5/projeto/dbcoffeetime/DbCoffeetimeApplication.java
@@ -0,0 +1,13 @@
+package g5.projeto.dbcoffeetime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbCoffeetimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbCoffeetimeApplication.class, args);
+	}
+
+}
