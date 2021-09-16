8fd7dd0a2f6be457a11a5b012ab3c85226ef90e0
Matheus <mhateus1995@gmail.com>
2021-09-16 00:16:31 -0300

    criacao de novas changes

---
+++ db-coffeetime/src/test/java/g5/projeto/dbcoffeetime/DbCoffeetimeApplicationTests.java
@@ -0,0 +1,13 @@
+package g5.projeto.dbcoffeetime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbCoffeetimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
