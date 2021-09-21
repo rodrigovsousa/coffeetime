d8d28050f800e91632142d999416cfeaec256fef
Erick Leone <leoneerick56@gmail.com>
2021-09-21 16:26:44 -0300

    remoção de imports dto

--- db-coffeetime/src/main/java/g5/projeto/dbcoffeetime/service/dto/MotivoDTO.java
+++ db-coffeetime/src/main/java/g5/projeto/dbcoffeetime/service/dto/MotivoDTO.java
@@ -3,7 +3,6 @@ package g5.projeto.dbcoffeetime.service.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
--- db-coffeetime/src/main/java/g5/projeto/dbcoffeetime/service/dto/SelectDTO.java
+++ db-coffeetime/src/main/java/g5/projeto/dbcoffeetime/service/dto/SelectDTO.java
@@ -3,9 +3,6 @@ package g5.projeto.dbcoffeetime.service.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
 @Getter
 @Setter
 public class SelectDTO {
