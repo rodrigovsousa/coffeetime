e88dca600c7865c611a1f72d925044d32c635a3d
Erick Leone <leoneerick56@gmail.com>
2021-09-23 14:58:58 -0300

    mudanças

--- db-coffeetime/src/main/java/g5/projeto/dbcoffeetime/service/dto/EmailDTO.java
+++ db-coffeetime/src/main/java/g5/projeto/dbcoffeetime/service/dto/EmailDTO.java
@@ -3,7 +3,6 @@ package g5.projeto.dbcoffeetime.service.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
