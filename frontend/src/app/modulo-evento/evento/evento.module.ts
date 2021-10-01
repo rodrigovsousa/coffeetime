a057c5e1a8d8ad4907fc0566a9ad9d8006fb6445
Matheus <mhateus1995@gmail.com>
2021-10-01 13:22:10 -0300

    inserindo evento-modulo

--- frontend/src/app/modulo-evento/evento/evento.module.ts
+++ frontend/src/app/modulo-evento/evento/evento.module.ts
@@ -1,12 +1,16 @@
+import { EventoRoutingModule } from './../evento-routing/evento-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    EventoRoutingModule,
+    SharedModule
   ]
 })
 export class EventoModule { }
